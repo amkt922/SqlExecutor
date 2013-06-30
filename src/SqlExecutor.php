7c36af389886f249dedeb34959537ae483c8dd21
amkt922 <amkt922@gmail.com>
2013-06-30 17:06:18 +0900

    initial commit

---
+++ src/SqlExecutor.php
@@ -0,0 +1,33 @@
+<?php
+namespace SqlExecutor;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of SqlExecutor
+ *
+ * @author amkt
+ */
+class SqlExecutor {
+    
+    /**
+     * constructor
+     */
+    public function __construct() {
+        ;
+    }
+    
+    /**
+     * 
+     * 
+     * @param string $sqlFile sql file name 
+     * @param array $params parameters for sql
+     */
+    public function from($sqlFile, $params = array()) {
+        
+    }
+}
+
---
+++ test/sql/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO user VALUES (1, 'joe', 'joepass');
+INSERT INTO user VALUES (2, 'sunny', 'sunnypass');
